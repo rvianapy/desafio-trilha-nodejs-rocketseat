e18a201629108c465adeceb4ca06962f69f38572
Rodrigo Viana <roviol@gmail.com>
2022-10-16 20:32:50 -0300

    feat: new user route created

--- src/index.js
+++ src/index.js
@@ -1,21 +1,44 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+  let user = {};
+  
+  const usernameAlreadyExists = users.some(user => user.username === username);
+    
+  if (usernameAlreadyExists) {
+    return response.status(400).json(
+      {
+        error: "Username already exists!"
+      }
+    )
+  } else {
+    user = {
+      id: uuidv4(),
+      name: name,
+      username: username,
+      todos: []
+    }
+  
+    users.push(user);
+  }
+
+  return response.json(user);
+
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
